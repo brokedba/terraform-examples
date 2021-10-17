91626c0653982c3b5620409d947930e8ba5ac20b
BrokeDBA <kouss.hd@gmail.com>
2021-10-17 17:48:05 -0400

    Add files via upload

---
+++ terraform-provider-gcp/create-vpc/outputs.tf
@@ -0,0 +1,39 @@
+output "vpc_name" {
+  description = "The Name of the newly created vpc"
+  value       = google_compute_network.terra_vpc.name
+}
+#output "vpc_id" {
+#      description = "id of created vpc. "
+#      value       = google_compute_network.terra_vpc.id
+#    } 
+    
+output "Subnet_Name" {
+      description = "Name of created vpc's Subnet. "
+      value       =  google_compute_subnetwork.terra_sub.name
+    }
+output "Subnet_id" {
+      description = "id of created vpc. "
+      value       = google_compute_subnetwork.terra_sub.id
+    }
+output "Subnet_CIDR" {
+      description = "cidr block of vpc's Subnet. "
+      value       = google_compute_subnetwork.terra_sub.ip_cidr_range
+    }
+
+output "firewall_Name" {
+       description = "Security Group Name. "
+       value       = google_compute_firewall.web-server.name
+   }
+ 
+output "fire_wall_rules" {
+      description = "Shows ingress rules of the Security group "
+     value       = google_compute_firewall.web-server.allow
+}           
+
+output "secondary_sub_ip_range" 
+      description = "Shows ingress rules of the Security group "
+      value       = google_compute_subnetwork.terra_sub.secondary_ip_range
+}           
+
+
+
---
+++ terraform-provider-gcp/create-vpc/variables.tf
@@ -0,0 +1,39 @@
+# GCP Service account region and authentication 
+# variable "prefix" {
+#  description = "The prefix used for all resources in this example"
+#}
+variable  "gcp_credentials"{
+  description = "default location of your service account json key file"
+  default = "~/gcp-key.json"
+}
+
+variable "project" {
+  default = "playground-s-11-83a5e4fc"
+}
+variable "region" {
+    default = "us-east1"
+}
+
+variable "zone" {
+    default = "us-east1-b"
+}
+# VPC INFO
+variable "vnet_name" {
+      default = "Terravpc"
+    }
+    
+variable "subnet-02_cidr" {
+      default = "192.168.0.0/16"
+    }
+
+# SUBNET INFO
+    variable "subnet_name"{
+      default = "terrasub" 
+      }
+
+    variable "subnet_cidr"{
+      default = "192.168.10.0/24"
+      } 
+  variable "firewall_name" {
+    default = "terra_fw"
+  }
