a04980b3823a485160488e784842340735866825
BrokeDBA <kouss.hd@gmail.com>
2021-12-19 06:32:28 -0500

    Add files via upload

--- terraform-provider-libvirt/kvm-compute.tf
+++ terraform-provider-libvirt/kvm-compute.tf
@@ -21,15 +21,27 @@ resource "libvirt_volume" "centos7-qcow2" {
   #source = "./CentOS-7-x86_64-GenericCloud.qcow2"
   format = "qcow2"
 }
+# Use CloudInit to add our ssh-key to the instance
+resource "libvirt_cloudinit_disk" "commoninit" {
+ name           = "commoninit.iso"
+ pool           = "default" #CHANGEME
+ user_data      = data.template_file.user_data.rendered
+}
 
+data "template_file" "user_data" {
+ template = file("${path.module}/cloud_init.cfg")
+}
 # Define KVM domain to create
-resource "libvirt_domain" "db1" {
-  name   = "db1"
+resource "libvirt_domain" "centovm" {
+  name   = "centovm"
   memory = "1024"
   vcpu   = 1
 
+cloudinit = libvirt_cloudinit_disk.commoninit.id
+
   network_interface {
     network_name = "default"
+    hostname = "terracentos"
   }
 
   disk {
