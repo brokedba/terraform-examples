34f8950d01e37a6d9e302844be60ce533d4f24d7
BrokeDBA <kouss.hd@gmail.com>
2023-04-30 09:02:52 -0400

    Update variables.tf

--- terraform-provider-oci/create-vcn/variables.tf
+++ terraform-provider-oci/create-vcn/variables.tf
@@ -3,7 +3,8 @@ variable "compartment_ocid" {}
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key_path" {  default = " "}
+variable "private_key" {}
 variable "region" {}
 
 variable "availability_domain_name" {
