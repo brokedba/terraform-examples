783a4a2962fe8272b20ece2acd5f027c3b7db776
BrokeDBA <kouss.hd@gmail.com>
2023-05-24 03:06:03 -0400

    Update vpc.tf

--- terraform-provider-gcp/create-vpc/vpc.tf
+++ terraform-provider-gcp/create-vpc/vpc.tf
@@ -1,5 +1,5 @@
 provider "google" {
-    credentials = file(var.gcp_credentials)
+  #  credentials = file(var.gcp_credentials)
     project = var.project 
     region  = var.region
     zone    = var.zone
